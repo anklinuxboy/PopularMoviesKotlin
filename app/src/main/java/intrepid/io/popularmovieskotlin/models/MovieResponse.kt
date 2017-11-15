faa7ff4e3844a85da8ec86bb5672828a32a57750
Ankit Sharma <ankit@intrepid.io>
2017-11-15 16:26:12 -0500

    DB working

---
+++ app/src/main/java/intrepid/io/popularmovieskotlin/models/MovieResponse.kt
@@ -0,0 +1,20 @@
+package intrepid.io.popularmovieskotlin.models
+
+import android.arch.persistence.room.ColumnInfo
+import android.arch.persistence.room.Entity
+import android.arch.persistence.room.Ignore
+import android.arch.persistence.room.PrimaryKey
+
+data class MovieResponse(val results: List<MovieInfo>)
+
+@Entity(tableName = "movies")
+data class MovieInfo(@PrimaryKey @ColumnInfo(name = "movie_id") var id : String,
+                     @Ignore var vote_average: String,
+                     @ColumnInfo(name = "movie_title") var title: String,
+                     @Ignore var poster_path: String,
+                     @ColumnInfo(name = "movie_overview") var overview: String,
+                     var release_date: String,
+                     @ColumnInfo(name = "movie_poster") var posterUrl: String,
+                     @ColumnInfo(name = "movie_rating") var voterRating: String) {
+    constructor() :this("", "", "", "", "", "", "", "")
+}
--- app/src/main/java/intrepid/io/popularmovieskotlin/repository/MovieRepository.kt
+++ app/src/main/java/intrepid/io/popularmovieskotlin/repository/MovieRepository.kt
@@ -1,28 +1,40 @@
 package intrepid.io.popularmovieskotlin.repository
 
 import android.arch.lifecycle.MutableLiveData
-import intrepid.io.popularmovieskotlin.BuildConfig
-import intrepid.io.popularmovieskotlin.MoviesApplication
-import intrepid.io.popularmovieskotlin.SORT_PREF
-import intrepid.io.popularmovieskotlin.data.MovieInfo
-import intrepid.io.popularmovieskotlin.data.MovieResponse
+import android.util.Log
+import intrepid.io.popularmovieskotlin.*
+import intrepid.io.popularmovieskotlin.db.MovieDao
+import intrepid.io.popularmovieskotlin.models.MovieInfo
+import intrepid.io.popularmovieskotlin.models.MovieResponse
 import intrepid.io.popularmovieskotlin.net.MovieService
+import io.reactivex.Completable
+import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
+import timber.log.Timber
 import javax.inject.Inject
 
 class MovieRepository {
     @Inject
     lateinit var movieService: MovieService
+    @Inject
+    lateinit var movieDao : MovieDao
 
-    var moviesData : MutableLiveData<ArrayList<MovieInfo>> = MutableLiveData()
+    val compositeDisposable = CompositeDisposable()
+
+    var moviesData: MutableLiveData<List<MovieInfo>> = MutableLiveData()
 
     init {
         MoviesApplication.appComponent.inject(this)
         getMovies()
     }
 
-    fun getMovies() {
+    private fun getMovies() {
+        getMoviesFromDB()
+    }
+
+    private fun getMoviesFromApi() {
         val movieApiCall = movieService.getMovies(SORT_PREF, BuildConfig.OPEN_TMDB_API_KEY)
 
         movieApiCall.subscribeOn(Schedulers.io())
@@ -30,10 +42,43 @@ class MovieRepository {
                 .map { response: MovieResponse? ->
                     return@map response?.results
                 }
-                .subscribe({ movieList: ArrayList<MovieInfo>? ->
+                .subscribe({ movieList: List<MovieInfo>? ->
                     kotlin.run {
+                        setPosterUrlRating(movieList)
                         moviesData.value = movieList
+                        insertMoviesInDB(movieList)
                     }
                 })
+    }
+
+    private fun getMoviesFromDB() {
+        compositeDisposable.add(Observable.fromCallable {movieDao.getSavedMovies()}
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe {
+                    if (it == null) {
+                        Log.d("test", "test movies not loaded from DB")
+                        getMoviesFromApi()
+                    } else {
+                        moviesData.value = it.value
+                        Log.d("test", "test movies loaded from db")
+                    }
+                })
+    }
+
+    private fun insertMoviesInDB(moviesList: List<MovieInfo>?) {
+        compositeDisposable.add(Observable.fromCallable { movieDao.saveMovies(moviesList!!)}
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe())
+    }
+
+    private fun setPosterUrlRating(movieList: List<MovieInfo>?) {
+        if (movieList != null) {
+            for (movie in movieList) {
+                movie.posterUrl = POSTER_URL + movie.poster_path
+                movie.voterRating = movie.vote_average + RATING
+            }
+        }
     }
 }
