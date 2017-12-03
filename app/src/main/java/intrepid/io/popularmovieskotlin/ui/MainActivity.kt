d0db4528aae20b289b07f75fd30ec796e79d3397
Ankit Sharma <ankit@intrepid.io>
2017-12-03 09:24:19 -0500

    Refactored package

--- app/src/main/java/intrepid/io/popularmovieskotlin/ui/MainActivity.kt
+++ app/src/main/java/intrepid/io/popularmovieskotlin/ui/MainActivity.kt
@@ -1,10 +1,11 @@
-package intrepid.io.popularmovieskotlin;
+package intrepid.io.popularmovieskotlin.ui;
 
 import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.GridView
+import intrepid.io.popularmovieskotlin.R
 import intrepid.io.popularmovieskotlin.adapters.GridViewAdapter
 import intrepid.io.popularmovieskotlin.models.MovieInfo
 import intrepid.io.popularmovieskotlin.viewmodels.MovieViewModel
