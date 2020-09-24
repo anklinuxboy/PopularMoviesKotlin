57c44b481b2798a34284b5edeb4f272644f616d2
Ankit Sharma <asharma4@wayfair.com>
2020-09-24 12:23:23 -0400

    Upgraded to Android X

--- README.md
+++
@@ -1,4 +0,0 @@
-# PopularMoviesKotlin
-A basic App which demonstrates how to use Kotlin with Android Arch Components with Retrofit 2, Rx 2, Dagger 2
-
-![Talk Poster](PosterTalk.jpg)
--- app/src/main/java/intrepid/io/popularmovieskotlin/Constants.kt
+++
@@ -1,6 +0,0 @@
-package intrepid.io.popularmovieskotlin
-
-const val API_BASE_URL = "http://api.themoviedb.org/3/"
-const val SORT_PREF = "popular"
-const val POSTER_URL = "http://image.tmdb.org/t/p/w185/"
-const val RATING = "/10"
